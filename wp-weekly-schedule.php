8e43444e21745360f9bc25ec559a590dfee3b2af
tanakamanabu <tm@lily-white.net>
2022-01-23 22:33:39 +0900

    新規作成

---
+++ wp-weekly-schedule.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: WpWeelkySchedule
+  Plugin URI: https://github.com/tanakamanabu/wp-weekly-schedule
+  Description: 週間スケジュールを管理・表示するプラグイン
+  Version: 1.0.0
+  Author: tanakamanabu
+  Author URI: https://github.com/tanakamanabu
+ */
+
+?>
